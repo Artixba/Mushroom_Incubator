7be719ed239242b6313615d80bdc5cc7e8f6902f
Jixba <Artixba@hotmail.com>
2020-04-04 08:05:40 -0500

    Improved the site's look overall by adding the following: -added a footer to go with the navbar -made each page significantly bigger -moved the login/logout, register, and the profile buttons to the right -added some styling to several divs

--- MushroomIncubator/mushroom_incubator/mushroom_site/admin.py
+++ MushroomIncubator/mushroom_incubator/mushroom_site/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from mushroom_site.models import UserProfileInfo, Contact
 
 # Register your models here.
+
+class MushroomAdmin(admin.ModelAdmin):
+    fields = ['last_name','first_name','email', 'message']
+    search_fields=["first_name","last_name"]
+    list_display = ['last_name','first_name','email', 'message']
+
 admin.site.register(UserProfileInfo)
-admin.site.register(Contact)
+admin.site.register(Contact,MushroomAdmin)
