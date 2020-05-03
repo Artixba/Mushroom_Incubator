246ea850ba11a325f107c74e3b74109e6d516041
Jixba <Artixba@hotmail.com>
2020-05-03 02:43:03 -0500

    Added more functionality to Sensor display

--- MushroomIncubator/mushroom_incubator/mushroom_site/static/js/blynk.js
+++ MushroomIncubator/mushroom_incubator/mushroom_site/static/js/blynk.js
@@ -1,41 +1,39 @@
-const auth_token = 'jWL3x5xRTSLnDnXZyuXxtNiznJ8ikLAm';
-const pin = 'D2';
+const AUTH_TOKEN = 'jWL3x5xRTSLnDnXZyuXxtNiznJ8ikLAm';
+const TEMP_PIN = 'D2';
+const HUM_PIN = 'D4';
+const CO2_PIN = 'D2';
+const LIGHT_PIN = 'D4';
+const TIME_PIN = 'V1';
+
 
 // http://blynk-cloud.com/auth_token/get/pin
 
-function Bbuttonhandler(){
+function queryAndSet(pin, elementId){
   var request = new XMLHttpRequest();
-  request.open('GET', 'http://blynk-cloud.com/jWL3x5xRTSLnDnXZyuXxtNiznJ8ikLAm/get/D2');
-
+  request.open('GET', `http://blynk-cloud.com/${AUTH_TOKEN}/get/${pin}`);
   request.onreadystatechange = function () {
     if (this.readyState === 4) {
       console.log('Status:', this.status);
       console.log('Headers:', this.getAllResponseHeaders());
       console.log('Body:', this.responseText);
-      document.getElementById("Blynk1").innerHTML = this.responseText;
+      document.getElementById(elementId).innerHTML = JSON.parse(this.responseText)[0];
     }
   };
   request.send();
 }
-document.getElementById("Bbutton").onclick = Bbuttonhandler;
- 
-function Abuttonhandler(){
-  var request = new XMLHttpRequest();
-  request.open('GET', 'http://blynk-cloud.com/jWL3x5xRTSLnDnXZyuXxtNiznJ8ikLAm/get/D4');
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      console.log('Status:', this.status);
-      console.log('Headers:', this.getAllResponseHeaders());
-      console.log('Body:', this.responseText);
-      document.getElementById("Blynk2").innerHTML = this.responseText;
-    }
-  };
-  request.send();
+queryAndSet(TEMP_PIN, "temp");
+queryAndSet(HUM_PIN, "hum");  
+document.getElementById("temp_hum_button").onclick = function() { 
+  queryAndSet(TEMP_PIN, "temp");
+  queryAndSet(HUM_PIN, "hum");  
 }
-document.getElementById("Abutton").onclick = Abuttonhandler;
-
-document.getElementById("Cbutton").onclick = function() { 
-  Abuttonhandler();
-  Bbuttonhandler();  
+queryAndSet(CO2_PIN, "co2");
+document.getElementById("co2_button").onclick = function() { 
+  queryAndSet(CO2_PIN, "co2");
+}
+queryAndSet(LIGHT_PIN, "light");
+  queryAndSet(TIME_PIN, "time");
+document.getElementById("light_button").onclick = function() { 
+  queryAndSet(LIGHT_PIN, "light");
+  queryAndSet(TIME_PIN, "time");  
 }
